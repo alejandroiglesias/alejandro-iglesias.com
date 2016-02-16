e374444c445b44aadc85df83d6764d4efc318924
Alejandro Iglesias <ale.garciaiglesias@gmail.com>
2016-02-16 15:27:04 -0300

    Added safer email address output; shortened Linkedin link

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+/*eslint no-unused-vars: 1*/
+(function() {
+  'use strict';
+
+  /**
+   * Anti-spam function to append link with email address into DOM.
+   *
+   * @param user    Username part of the email address.
+   * @param domain  Domain part of the email address.
+   */
+  window.email = function(user, domain) {
+    document.write('<a href="mailto:' + user + '@' + domain + '">' + user + '@' + domain + '</a>');
+  };
+
+}());
